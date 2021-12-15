6b3d71298f82829460f1998b24657baac0395727
fauzi1999mei <riskysingaraja2018@gmail.com>
2021-12-15 20:09:04 +0800

    Login Done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserController extends Controller
+{
+    function login(Request $req)
+    {
+        // return $req->input();
+        $user =  User::where(['email'=>$req->email])->first();
+        if(!$user || !Hash::check($req->password, $user -> password))
+        {
+            return "Username or password does not match";
+        }
+        else
+        {
+            $req -> session()->put('user', $user);
+            return redirect('/');
+        }
+    }
+}
